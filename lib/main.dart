f3fff1d1de01f086b70dec69e34fb164806e85aa
deepanshu <greekof022@gmail.com>
2024-05-28 10:55:10 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+part 'login_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,12 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('hello world'),
-        ),
-      ),
-    );
+    return const LoginPage();
   }
 }
